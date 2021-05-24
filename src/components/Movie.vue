e946e91615b7637317b0513d004d421fe1a982b8
Francisco Reza <fran.js_@hotmail.com>
2021-05-24 08:42:17 -0500

    omg

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -8,7 +8,7 @@
                         <span class="grey--text"> {{ movie.release_year }} . {{ movie.genre }} </span>
                     </div>
                 </v-card-title>
-                <h6 class="card-title">Rese&ntilde;ar pelicula</h6>
+                <h6 class="card-title" v-if="current_user">Rese&ntilde;ar pelicula</h6>
                 <v-card-text>
                     {{ movie.description }}
                 </v-card-text>
@@ -18,6 +18,32 @@
 </template>
 <script>
 import axios from 'axios';
+import Vue from 'vue';
+import StarRating from 'vue-star-rating';
+
+const wrapper = document.createElement('div');
+const state = {
+    note: 0,
+};
+
+const RatingComponent = Vue.extend({
+    data() {
+        return { rating: 0 };
+    },
+    watch: {
+        rating(newVal) { state.note = newVal; },
+    },
+    template: `
+    <div class="rating">
+        Que les parecio esta pelicula?
+        <star-rating v-model="rating" :show-rating="false"></star-rating>
+    </div>`,
+    components: { 'star-ratings': StarRating },
+});
+
+const component = new RatingComponent().$mount(wrapper);
+ 
+
 export default {
     name: 'Movie',
     data() {
@@ -29,6 +55,18 @@ export default {
         this.fetchMovie();
     },
     methods: {
+        async rate() {
+            this.$swal({
+                content: component.$e1,
+                buttons:{
+                    confirm:{
+                        value: 0,
+                    },
+                },
+            }).then(() => {
+                
+            })
+        },
         async fetchMovie(){
             return axios({
                 method: 'get',
